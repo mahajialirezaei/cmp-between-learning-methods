ec5f69c416cf766ae465f5c38a17852c6e179b2f
Mohammad <m.a.hajialirezaei05@gmail.com>
2025-06-28 19:56:25 +0330

    update pre_processing

--- pre_processing_data.py
+++ pre_processing_data.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
+from imblearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, OrdinalEncoder
+from sklearn.preprocessing import StandardScaler, OrdinalEncoder, OneHotEncoder
 from sklearn.impute import SimpleImputer
 
 
@@ -10,20 +12,30 @@ def pre_processing():
     X = data.drop('target', axis=1)
     y = data['target']
 
+    # تقسیم داده
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.3, stratify=y, random_state=42)
 
-    num_imputer = SimpleImputer(strategy='median')
-    cat_imputer = SimpleImputer(strategy='most_frequent')
+    # تعریف ستون‌های عددی و طبقه‌ای
+    numeric_features = ['transaction_amount', 'transaction_time', 'customer_history',
+                        'feature4', 'feature5', 'feature6']
+    categorical_features = ['merchant_category', 'location_mismatch', 'device_used']
 
-    encoder = OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1)
-    X_train_cat = encoder.fit_transform(X_train.select_dtypes(include=['object']))
-    X_test_cat = encoder.transform(X_test.select_dtypes(include=['object']))
+    # ایجاد pipeline پیش‌پردازش
+    numeric_transformer = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='median')),
+        ('scaler', StandardScaler())])
 
-    scaler = StandardScaler()
-    X_train_num = scaler.fit_transform(X_train.select_dtypes(exclude=['object']))
-    X_test_num = scaler.transform(X_test.select_dtypes(exclude=['object']))
+    categorical_transformer = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='most_frequent')),
+        ('onehot', OneHotEncoder(handle_unknown='ignore'))])
 
-    X_train_processed = np.concatenate([X_train_num, X_train_cat], axis=1)
-    X_test_processed = np.concatenate([X_test_num, X_test_cat], axis=1)
-    return X_train_processed, X_test_processed, y_train, y_test, num_imputer, cat_imputer
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', numeric_transformer, numeric_features),
+            ('cat', categorical_transformer, categorical_features)])
+
+    # اعمال پیش‌پردازش
+    X_train_processed = preprocessor.fit_transform(X_train)
+    X_test_processed = preprocessor.transform(X_test)
+    return X_train_processed, X_test_processed, y_train, y_test
