c5c66f573b58d2323ae703bfd20b4b95b5806bba
Mohammad <m.a.hajialirezaei05@gmail.com>
2025-06-28 19:46:42 +0330

    define_models

--- deal_with_imbalance.py
+++ deal_with_imbalance.py
@@ -13,3 +13,46 @@ def smote_tech(X_train_processed, y_train):
 
     smote = SMOTE(sampling_strategy='auto', k_neighbors=5, random_state=42)
     X_train_smote, y_train_smote = smote.fit_resample(X_train_processed, y_train)
+
+
+
+def define_models(X_train_processed, y_train, X_train_smote, y_train_smote, class_weights, weights):
+    models = {
+        'Random Forest': RandomForestClassifier(
+            n_estimators=100,
+            class_weight='balanced',
+            random_state=42
+        ),
+        'XGBoost': XGBClassifier(
+            n_estimators=100,
+            scale_pos_weight=weights[1] / weights[0],  # برای کلاس اقلیت
+            random_state=42,
+            eval_metric='aucpr'
+        ),
+        'LightGBM': LGBMClassifier(
+            n_estimators=100,
+            class_weight='balanced',
+            random_state=42,
+            objective='binary'
+        ),
+        'CatBoost': CatBoostClassifier(
+            iterations=100,
+            auto_class_weights='Balanced',
+            random_seed=42,
+            verbose=0
+        )
+    }
+
+    for name, model in models.items():
+        model.fit(X_train_processed, y_train)
+
+        if name != 'CatBoost':
+            model.set_params(**{'class_weight': class_weights})
+        model.fit(X_train_processed, y_train)
+
+        model.fit(X_train_smote, y_train_smote)
+
+        model.set_params(**{'class_weight': None})
+        model.fit(X_train_smote, y_train_smote)
+
+
